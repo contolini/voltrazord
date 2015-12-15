8805d3e7ac918d2704b3b86909601843a295d407
Chris Contolini <contolini@users.noreply.github.com>
2015-12-14 23:55:46 -0500

    :arrow_lower_right:

--- scripts/npm/prepublish/index.js
+++ scripts/npm/prepublish/index.js
@@ -165,20 +165,20 @@ function publishComponents() {
 }
 
 function commit(result) {
-  if (!result || !result.stdout) return;
-  util.printLn.console(result.stdout);
+  if (!componentsToPublish.length) return;
+  if (result && result.stdout) util.printLn.console(result.stdout);
   return util.git.commit(util.pkg.version);
 }
 
 function push(result) {
-  if (!result || !result.stdout) return;
-  util.printLn.console(result.stdout);
+  if (!componentsToPublish.length) return;
+  if (result && result.stdout) util.printLn.console(result.stdout);
   return util.git.push();
 }
 
 function finish(result) {
-  if (!result || !result.stdout) return;
-  util.printLn.console(result.stdout);
+  if (!componentsToPublish.length) return;
+  if (result && result.stdout) util.printLn.console(result.stdout);
   util.printLn.success('Hooray! All done!');
   process.exit(0);
 }
